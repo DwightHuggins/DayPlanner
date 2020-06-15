6a002bfb019937ae0387ae82ad66ea3c544877a1
Dwight Huggins <dwighthuggins@DwightHugginsJr.local>
2020-06-15 17:50:52 -0400

    updated data and time on html file

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
 
 var plannerContent = JSON.parse(localStorage.getItem("planner-items")) || {};
-
 
 const timeBlockDiv = $("#time-block");
 
